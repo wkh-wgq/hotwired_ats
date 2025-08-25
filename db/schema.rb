be08a77c6be9d28ef7b151be64e35958fc26ae91
wangkaihua <16621142402@163.com>
2025-08-25 15:57:35 +0800

    完成chapters-1

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 0) do
+ActiveRecord::Schema[7.2].define(version: 2025_08_25_073721) do
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
 end
